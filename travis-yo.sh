438a60ed44dbcca3e7823e3c1b9d2794fd6fe67e
Dan Smith <dev@dotherightthing.co.nz>
2018-03-27 01:39:22 +1300

    Don't use spawn

--- travis-yo.sh
+++ travis-yo.sh
@@ -5,7 +5,7 @@ mkdir 'wpdtrt-generated-plugin'
 
 cd wpdtrt-generated-plugin
 
-spawn yo wp-plugin-boilerplate
+yo wp-plugin-boilerplate
 
 expect "? Plugin name (file safe) (wpdtrt-generated-plugin)"
 send "\r"
