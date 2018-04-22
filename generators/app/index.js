742ce4bc5204aabe8ef8ae3ae8940e1483ee5ef5
Dan Smith <dev@dotherightthing.co.nz>
2018-04-22 21:21:06 +1200

    Improve prompt wording

--- generators/app/index.js
+++ generators/app/index.js
@@ -350,37 +350,37 @@ module.exports = class extends Generator {
             {
                 type: 'input',
                 name: 'localTestDatabaseName',
-                message: 'Database name for WordPress Unit tests',
+                message: 'Database name for WordPress Unit tests (local dev)',
                 default: this.config.get('localTestDatabaseName')
             },
             {
                 type: 'input',
                 name: 'localTestDatabaseUserName',
-                message: 'Database user name for WordPress Unit tests',
+                message: 'Database user for WordPress Unit tests (local dev)',
                 default: this.config.get('localTestDatabaseUserName')
             },
             {
                 type: 'input',
                 name: 'localTestDatabasePassword',
-                message: 'Database password for WordPress Unit tests',
+                message: 'Database password for WordPress Unit tests (local dev)',
                 default: this.config.get('localTestDatabasePassword')
             },
             {
                 type: 'input',
                 name: 'travisTestDatabaseName',
-                message: 'Database name for WordPress Unit tests run by Travis CI',
+                message: 'Database name for WordPress Unit tests (Travis CI)',
                 default: this.config.get('travisTestDatabaseName')
             },
             {
                 type: 'input',
                 name: 'travisTestDatabaseUserName',
-                message: 'Database user name for WordPress Unit tests run by Travis CI',
+                message: 'Database user for WordPress Unit tests (Travis CI)',
                 default: this.config.get('travisTestDatabaseUserName')
             },
             {
                 type: 'input',
                 name: 'travisTestDatabasePassword',
-                message: 'Database password for WordPress Unit tests run by Travis CI',
+                message: 'Database password for WordPress Unit tests (Travis CI)',
                 default: this.config.get('travisTestDatabasePassword')
             }
         ];
