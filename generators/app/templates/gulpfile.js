9dfdb61e829f48727cca36b4f9b0aa80e02e05c3
Dan Smith <dev@dotherightthing.co.nz>
2018-06-11 20:34:34 +1200

    Fix "The @package tag does not permit a value;" (dotherightthing/wpdtrt-plugin-boilerplate#129)

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -5,7 +5,6 @@
  * @example usage from child plugin:
  *    gulp --gulpfile ./vendor/dotherightthing/wpdtrt-plugin-boilerplate/gulpfile.js --cwd ./
  *
- * @package <%= nameFriendlySafe %>
  * @version <%= defaultVersion %>
  * @since   <%= generatorVersion %> DTRT WordPress Plugin Boilerplate Generator
  */
