300c6e47b748b1e923d4e9aef2253c285cb8a40c
Dan Smith <dev@dotherightthing.co.nz>
2018-06-25 00:23:24 +1200

    Refactor JS to generate meaningful JSDoc output (#36)

--- generators/app/templates/js/frontend.js
+++ generators/app/templates/js/frontend.js
@@ -1,8 +1,8 @@
 /**
- * Scripts for the public front-end
- *
- * PHP variables are provided in <%= nameSafe %>_config.
- *
+ * @file <%= nameFriendly %> frontend.js
+ * @summary
+ *     Front-end scripting for public pages
+ *     PHP variables are provided in `<%= nameSafe %>_config`.
  * @version <%= defaultVersion %>
  * @since   <%= generatorVersion %> DTRT WordPress Plugin Boilerplate Generator
  */
@@ -10,11 +10,24 @@
 /* eslint-env browser */
 /* global document, $, jQuery, <%= nameSafe %>_config */
 
-/*
+/**
+ * @namespace <%= nameSafe %>_ui
+ */
+var <%= nameSafe %>_ui = {
+
+    /**
+     * Initialise front-end scripting
+     * @since <%= defaultVersion %>
+     */
+    init: function() {
+        console.log("<%= nameSafe %>_ui.init");
+    }
+}
+
 jQuery(document).ready(function ($) {
 
     "use strict";
 
     var config = <%= nameSafe %>_config;
+    <%= nameSafe %>_ui.init();
 });
-*/
