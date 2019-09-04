0c037e2248a8d905ca93be58347f7a0eadefaf51
Dan Smith <dev@dotherightthing.co.nz>
2019-09-04 19:33:22 +1200

    Only press enter after a Yeoman prompt

--- expect.sh
+++ expect.sh
@@ -5,10 +5,19 @@
 # to accept the default answers.
 # http://www.tcl.tk/man/expect5.31/expect.1.html
 
-set timeout 1
-
 spawn yo wpdtrt-plugin-boilerplate 
 
 expect {
-    timeout {send "\r"; exp_continue}
+    "Plugin name" {send "\r"; exp_continue}
+    "Plugin description" {send "\r"; exp_continue}
+    "Plugin tags" {send "\r"; exp_continue}
+    "Software version" {send "\r"; exp_continue}
+    "Author names (first and last)" {send "\r"; exp_continue}
+    "Author name (GitHub.com)" {send "\r"; exp_continue}
+    "Author name (WordPress.org)" {send "\r"; exp_continue}
+    "Author abbreviation (no spaces)" {send "\r"; exp_continue}
+    "Author email (general)" {send "\r"; exp_continue}
+    "Author email (plugin support)" {send "\r"; exp_continue}
+    "Author donation URL" {send "\r"; exp_continue}
+    "Base URL for testing web pages in Cypress.io" {send "\r";}
 }
