52868899e1deb42a883af16efab68635f50393fc
Dan Smith <dev@dotherightthing.co.nz>
2019-09-04 19:56:44 +1200

    Wait 1 second before hitting enter

--- expect.sh
+++ expect.sh
@@ -8,16 +8,16 @@
 spawn yo wpdtrt-plugin-boilerplate 
 
 expect {
-    "Plugin name" {send "\r"; exp_continue}
-    "Plugin description" {send "\r"; exp_continue}
-    "Plugin tags" {send "\r"; exp_continue}
-    "Software version" {send "\r"; exp_continue}
-    "Author names (first and last)" {send "\r"; exp_continue}
-    "Author name (GitHub.com)" {send "\r"; exp_continue}
-    "Author name (WordPress.org)" {send "\r"; exp_continue}
-    "Author abbreviation (no spaces)" {send "\r"; exp_continue}
-    "Author email (general)" {send "\r"; exp_continue}
-    "Author email (plugin support)" {send "\r"; exp_continue}
-    "Author donation URL" {send "\r"; exp_continue}
-    "Base URL for testing web pages in Cypress.io" {send "\r";}
+    "Plugin name" {sleep 1; send "\r"; exp_continue}
+    "Plugin description" {sleep 1; send "\r"; exp_continue}
+    "Plugin tags" {sleep 1; send "\r"; exp_continue}
+    "Software version" {sleep 1; send "\r"; exp_continue}
+    "Author names (first and last)" {sleep 1; send "\r"; exp_continue}
+    "Author name (GitHub.com)" {sleep 1; send "\r"; exp_continue}
+    "Author name (WordPress.org)" {sleep 1; send "\r"; exp_continue}
+    "Author abbreviation (no spaces)" {sleep 1; send "\r"; exp_continue}
+    "Author email (general)" {sleep 1; send "\r"; exp_continue}
+    "Author email (plugin support)" {sleep 1; send "\r"; exp_continue}
+    "Author donation URL" {sleep 1; send "\r"; exp_continue}
+    "Base URL for testing web pages in Cypress.io" {sleep 1; send "\r";}
 }
