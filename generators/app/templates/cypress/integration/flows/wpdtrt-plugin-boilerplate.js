72af258d18c6a94b38939ff9425c035f8892422c
Dan Smith <dev@dotherightthing.co.nz>
2021-01-27 23:34:40 +1300

    Lint JS

--- generators/app/templates/cypress/integration/flows/wpdtrt-plugin-boilerplate.js
+++ generators/app/templates/cypress/integration/flows/wpdtrt-plugin-boilerplate.js
@@ -25,75 +25,75 @@
 
 const componentId = 'signposts';
 
-describe( 'Test Name', function () {
-  before( function () {
-    // load local web page
-    cy.visit( '/path/to/page' );
-  } );
+describe('Test Name', function () {
+    before(function () {
+        // load local web page
+        cy.visit('/path/to/page');
+    });
 
-  beforeEach( function () {
-    // refresh the page to reset the UI state
-    cy.reload();
+    beforeEach(function () {
+        // refresh the page to reset the UI state
+        cy.reload();
 
-    // @aliases
-    cy.get( `#${ componentId }` ).as( 'componentName' );
-    cy.get( `#${ componentId } .child` ).as( 'componentChildName' );
+        // @aliases
+        cy.get(`#${ componentId }`).as('componentName');
+        cy.get(`#${ componentId } .child`).as('componentChildName');
 
-    // default viewer attributes
-    cy.get( '@componentChildName' )
-      .should( 'have.attr', 'id', 'foo' )
-      .should( 'have.attr', 'data-bar', 'false' )
-      .should( 'not.have.attr', 'data-baz' );
+        // default viewer attributes
+        cy.get('@componentChildName')
+            .should('have.attr', 'id', 'foo')
+            .should('have.attr', 'data-bar', 'false')
+            .should('not.have.attr', 'data-baz');
 
-    // scroll component into view,
-    // as Cypress can't always 'see' elements below the fold
-    cy.get( '@componentName' )
-      .scrollIntoView( {
-        offset: {
-          top: 100,
-          left: 0
-        }
-      } )
-      .should( 'be.visible' );
+        // scroll component into view,
+        // as Cypress can't always 'see' elements below the fold
+        cy.get('@componentName')
+            .scrollIntoView({
+                offset: {
+                    top: 100,
+                    left: 0
+                }
+            })
+            .should('be.visible');
 
-    // @aliases for injected elements
-    cy.get( `#${ componentId } .child-injected` ).as( 'componentInjected' );
-  } );
+        // @aliases for injected elements
+        cy.get(`#${ componentId } .child-injected`).as('componentInjected');
+    });
 
-  describe( 'Setup', function () {
-    it( 'Has prerequisites', function () {
-      // check that the plugin object is available
-      cy.window().should( 'have.property', '<%= nameSafe %>_ui' );
+    describe('Setup', function () {
+        it('Has prerequisites', function () {
+            // check that the plugin object is available
+            cy.window().should('have.property', '<%= nameSafe %>_ui');
 
-      // check that it's an object
-      cy.window().then( ( win ) => {
-        expect( win.<%= nameSafe %>_ui ).to.be.a( 'object' );
-      } );
-    } );
-  } );
+            // check that it's an object
+            cy.window().then((win) => {
+                expect(win.<%= nameSafe %>_ui).to.be.a('object');
+            });
+        });
+    });
 
-  describe( 'Load', function () {
-    it( 'Loads', function () {
-      // check that the child component has been assigned the correct ID
-      cy.get( '@componentChildName' )
-        .should( 'have.attr', 'id', `${ componentId }-child` )
-        .should( 'have.attr', 'data-bar', 'true' )
-        .should( 'not.have.attr', 'data-baz' );
+    describe('Load', function () {
+        it('Loads', function () {
+            // check that the child component has been assigned the correct ID
+            cy.get('@componentChildName')
+                .should('have.attr', 'id', `${ componentId }-child`)
+                .should('have.attr', 'data-bar', 'true')
+                .should('not.have.attr', 'data-baz');
 
-      // check that the injected child component has the correct attributes and text
-      cy.get( '@componentInjected' )
-        .should( 'have.attr', 'aria-controls', `${ componentId }-child` )
-        .contains( 'Sweet child of mine' );
+            // check that the injected child component has the correct attributes and text
+            cy.get('@componentInjected')
+                .should('have.attr', 'aria-controls', `${ componentId }-child`)
+                .contains('Sweet child of mine');
 
-      // test the accessibility of the component state using Tenon.io
-      // Note: add a wrapper around the component so that the HTML can be submitted independently
-      // and in its entirety
-      cy.get( '@componentName' ).then( ( componentName ) => {
-        // testing the contents rather than the length gives a more useful error object
-        cy.task( 'tenonAnalyzeHtml', `${componentName.html()}` )
-          // an empty resultSet indicates that there are no errors
-          .its( 'resultSet' ).should( 'be.empty' );
-      } );
-    } );
-  } );
-} );
+            // test the accessibility of the component state using Tenon.io
+            // Note: add a wrapper around the component so that the HTML can be submitted independently
+            // and in its entirety
+            cy.get('@componentName').then((componentName) => {
+                // testing the contents rather than the length gives a more useful error object
+                cy.task('tenonAnalyzeHtml', `${componentName.html()}`)
+                // an empty resultSet indicates that there are no errors
+                .its('resultSet').should('be.empty');
+            });
+        });
+    });
+});
--- generators/app/templates/cypress/plugins/index.js
+++ generators/app/templates/cypress/plugins/index.js
@@ -10,94 +10,100 @@
 /* eslint-env node */
 /* globals Promise */
 
-const normalizeWhitespace = require( 'normalize-html-whitespace' );
-const TenonNode = require( 'tenon-node' );
+const normalizeWhitespace = require('normalize-html-whitespace');
+const TenonNode = require('tenon-node');
 
 // export a function
-module.exports = ( on ) => {
-  // configure plugins here
-  on( 'task', {
-    /**
-     * Lint a URL in Tenon
-     *
-     * Parameters:
-     *   (string) url - Fully qualified URL
-     *
-     * Returns:
-     *   (object) - Tenon response object
-     *
-     * See:
-     * - <tenonCommands: https://github.com/poorgeek/tenon-selenium-example/blob/master/test/helpers/tenonCommands.js>
-     * - <tenon-node: https://www.npmjs.com/package/tenon-node>
-     * - <Cypress.io Front End Unit Tests: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Testing-&-Debugging#d-cypressio-front-end-unit-tests>
-     */
-    tenonAnalyzeUrl( url ) {
-      const tenonApi = new TenonNode( {
-        key: process.env.TENON_API_KEY
-      } );
+module.exports = (on) => {
+    // configure plugins here
+    on('task', {
+        /**
+         * Lint a URL in Tenon
+         *
+         * Parameters:
+         *   (string) url - Fully qualified URL
+         *
+         * Returns:
+         *   (object) - Tenon response object
+         *
+         * See:
+         * - <tenonCommands: https://github.com/poorgeek/tenon-selenium-example/blob/master/test/helpers/tenonCommands.js>
+         * - <tenon-node: https://www.npmjs.com/package/tenon-node>
+         * - <Cypress.io Front End Unit Tests: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Testing-&-Debugging#d-cypressio-front-end-unit-tests>
+         *
+         * @param {string} url - Fully qualified URL
+         * @returns {object} - Tenon response object
+         */
+        tenonAnalyzeUrl(url) {
+            const tenonApi = new TenonNode({
+                key: process.env.TENON_API_KEY
+            });
 
-      return new Promise( ( resolve, reject ) => {
-        tenonApi.analyze( url, ( err, tenonResult ) => {
-          if ( err ) {
-            reject( err );
-          }
+            return new Promise((resolve, reject) => {
+                tenonApi.analyze(url, (err, tenonResult) => {
+                    if (err) {
+                        reject(err);
+                    }
 
-          if ( tenonResult.status > 400 ) {
-            reject( tenonResult.info );
-          } else {
-            resolve( tenonResult );
-          }
-        } );
-      } );
-    },
+                    if (tenonResult.status > 400) {
+                        reject(tenonResult.info);
+                    } else {
+                        resolve(tenonResult);
+                    }
+                });
+            });
+        },
 
-    /**
-     * Lint an HTML fragment in Tenon
-     *
-     * Parameters:
-     *   (string) selectorHtml - HTML fragment
-     *
-     * Returns:
-     *   (object) - Tenon response object
-     *
-     * See:
-     * - <tenonCommands: https://github.com/poorgeek/tenon-selenium-example/blob/master/test/helpers/tenonCommands.js>
-     * - <tenon-node: https://www.npmjs.com/package/tenon-node>
-     * - <Cypress.io Front End Unit Tests: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Testing-&-Debugging#d-cypressio-front-end-unit-tests>
-     *
-     * Example:
-     * --- js
-     * cy.task( 'tenonAnalyzeHtml', `${myElement.wrap( '<div/>' ).parent().html()}` )
-     *    .its( 'results' ).should( 'eq', [] );
-     * ---
-     */
-    tenonAnalyzeHtml( selectorHtml ) {
-      const html = normalizeWhitespace( selectorHtml ); // strip whitespace between html tags
-      const tenonApi = new TenonNode( {
-        key: process.env.TENON_API_KEY
-      } );
+        /**
+         * Lint an HTML fragment in Tenon
+         *
+         * Parameters:
+         *   (string) selectorHtml - HTML fragment
+         *
+         * Returns:
+         *   (object) - Tenon response object
+         *
+         * See:
+         * - <tenonCommands: https://github.com/poorgeek/tenon-selenium-example/blob/master/test/helpers/tenonCommands.js>
+         * - <tenon-node: https://www.npmjs.com/package/tenon-node>
+         * - <Cypress.io Front End Unit Tests: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Testing-&-Debugging#d-cypressio-front-end-unit-tests>
+         *
+         * Example:
+         * --- js
+         * cy.task('tenonAnalyzeHtml', `${myElement.wrap('<div/>').parent().html()}`)
+         *    .its('results').should('eq', []);
+         * ---
+         *
+         * @param {string} selectorHtml - HTML fragment
+         * @returns {object} - Tenon response object
+         */
+        tenonAnalyzeHtml(selectorHtml) {
+            const html = normalizeWhitespace(selectorHtml); // strip whitespace between html tags
+            const tenonApi = new TenonNode({
+                key: process.env.TENON_API_KEY
+            });
 
-      return new Promise( ( resolve, reject ) => {
-        tenonApi.analyze( html, { fragment: '1', level: 'AAA' }, ( err, tenonResponse ) => {
-          if ( err ) {
-            reject( new Error( err ) );
-          }
+            return new Promise((resolve, reject) => {
+                tenonApi.analyze(html, { fragment: '1', level: 'AAA' }, (err, tenonResponse) => {
+                    if (err) {
+                        reject(new Error(err));
+                    }
 
-          if ( tenonResponse.status === 401 ) {
-            reject( new Error( `${tenonResponse.status} Authentication Error. Please check your API key.` ) );
-          } else if ( tenonResponse.status > 400 ) {
-            reject( new Error( `${tenonResponse.status} Error` ) );
-          } else {
-            const dtrtResponse = {};
-            // list the issues
-            dtrtResponse.results = tenonResponse.resultSet.map( result => {
-              return `${ result.errorTitle }`;
-            } );
+                    if (tenonResponse.status === 401) {
+                        reject(new Error(`${tenonResponse.status} Authentication Error. Please check your API key.`));
+                    } else if (tenonResponse.status > 400) {
+                        reject(new Error(`${tenonResponse.status} Error`));
+                    } else {
+                        const dtrtResponse = {};
+                        // list the issues
+                        dtrtResponse.results = tenonResponse.resultSet.map(result => {
+                            return `${result.errorTitle}`;
+                        });
 
-            resolve( dtrtResponse );
-          }
-        } );
-      } );
-    }
-  } );
+                        resolve(dtrtResponse);
+                    }
+                });
+            });
+        }
+    });
 };
